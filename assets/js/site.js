f4f89474421aad85d291e3b056c68fb28fb019df
MarlaRey <hejmarlenehej@gmail.com>
2023-09-07 12:39:27 +0200

    visualisering
    styr på search med forbogstavet. der kommer et billede frem af retterne og man kan klikke på dem og få opskriften frem i en modal

--- assets/js/site.js
+++ assets/js/site.js
@@ -33,9 +33,181 @@ myIdSearchButton.addEventListener("click", () => {
 //-------------------------------------------------------------------------------------
 
 // fetch functions --------------------------------------------------------------------
-// your code goes here
 
 
+function getRecipiesByName(myName) {
+  apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${myName}`;
+
+  fetch(apiUrl)
+    .then((response) => {
+     
+        // error checking
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+
+      return response.json(); // Parse the response body as JSON
+    })
+
+    .then((data) => {
+      // send data on to view functions
+      setupResultView(data);
+    })
+
+    .catch((error) => {
+      serchMode = "error";
+      console.error("Error:", error);
+      setupResultView(error);
+    });
+}
+
+//billedevisning
+document.addEventListener('DOMContentLoaded', () => {
+  const firstLetterInput = document.getElementById('firstLetterInput');
+  const firstLetterSearch = document.getElementById('firstLetterSearch');
+  const myResult = document.getElementById('myResult');
+
+  // Function to display the recipe for a meal
+  function displayRecipe(mealId) {
+      // Fetch the recipe details from the API using the meal ID
+      fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+          .then((response) => response.json())
+          .then((data) => {
+              const meal = data.meals[0];
+
+              // Create a modal to display the recipe
+              const modal = document.createElement('div');
+              modal.classList.add('modal');
+
+              // Create a close button for the modal
+              const closeButton = document.createElement('button');
+              closeButton.textContent = 'Close';
+              closeButton.classList.add('close-button');
+              closeButton.addEventListener('click', () => {
+                  modal.remove();
+              });
+
+              // Create a heading for the recipe
+              const recipeHeading = document.createElement('h2');
+              recipeHeading.textContent = `Recipe for ${meal.strMeal}`;
+
+              // Create a paragraph for the recipe instructions
+              const recipeInstructions = document.createElement('p');
+              recipeInstructions.textContent = meal.strInstructions;
+
+              // Append elements to the modal
+              modal.appendChild(closeButton);
+              modal.appendChild(recipeHeading);
+              modal.appendChild(recipeInstructions);
+
+              // Append the modal to the body
+              document.body.appendChild(modal);
+          })
+          .catch((error) => {
+              console.error('Error fetching recipe data:', error);
+              alert('An error occurred while fetching recipe data.');
+          });
+  }
+
+  firstLetterSearch.addEventListener('click', () => {
+      const firstLetter = firstLetterInput.value;
+
+      // Check if the input is not empty
+      if (firstLetter.trim() === '') {
+          alert('Please enter a letter.');
+          return;
+      }
+
+      // Clear previous results
+      myResult.innerHTML = '';
+
+      // Fetch data from the API
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`)
+          .then((response) => response.json())
+          .then((data) => {
+              if (data.meals === null) {
+                  myResult.innerHTML = 'No meals found with that letter.';
+              } else {
+                  data.meals.forEach((meal) => {
+                      // Create a div for each meal
+                      const mealDiv = document.createElement('div');
+                      mealDiv.classList.add('meal-item');
+
+                      // Create an image element for the meal
+                      const mealImage = document.createElement('img');
+                      mealImage.src = meal.strMealThumb;
+                      mealImage.alt = meal.strMeal;
+
+                      // Make the image clickable to display the recipe
+                      mealImage.addEventListener('click', () => {
+                          displayRecipe(meal.idMeal);
+                      });
+
+                      // Create a paragraph for the meal name
+                      const mealName = document.createElement('p');
+                      mealName.textContent = meal.strMeal;
+
+                      // Append the image and name to the div
+                      mealDiv.appendChild(mealImage);
+                      mealDiv.appendChild(mealName);
+
+                      // Append the meal div to the result
+                      myResult.appendChild(mealDiv);
+                  });
+              }
+          })
+          .catch((error) => {
+              console.error('Error fetching data:', error);
+              myResult.innerHTML = 'An error occurred while fetching data.';
+          });
+
+      // Add a class to the article for styling
+      myResult.classList.add('gallery');
+  });
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const firstLetterInput = document.getElementById('firstLetterInput');
+//   const firstLetterSearch = document.getElementById('firstLetterSearch');
+//   const myResult = document.getElementById('myResult');
+
+//   firstLetterSearch.addEventListener('click', () => {
+//       const firstLetter = firstLetterInput.value;
+
+//       // Check if the input is not empty
+//       if (firstLetter.trim() === '') {
+//           alert('Please enter a letter.');
+//           return;
+//       }
+
+//       // Clear previous results
+//       myResult.innerHTML = '';
+
+//       // Fetch data from the API
+//       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`)
+//           .then((response) => response.json())
+//           .then((data) => {
+//               if (data.meals === null) {
+//                   myResult.innerHTML = 'No meals found with that letter.';
+//               } else {
+//                   // Create a list of meals
+//                   const mealsList = document.createElement('ul');
+
+//                   data.meals.forEach((meal) => {
+//                       const listItem = document.createElement('li');
+//                       listItem.textContent = meal.strMeal;
+//                       mealsList.appendChild(listItem);
+//                   });
+
+//                   myResult.appendChild(mealsList);
+//               }
+//           })
+//           .catch((error) => {
+//               console.error('Error fetching data:', error);
+//               myResult.innerHTML = 'An error occurred while fetching data.';
+//           });
+//   });
+// });
 
 
 
@@ -48,7 +220,11 @@ function setupResultView(myData) {
   switch (serchMode) {
     case "firstLetterSearch":
       console.log(myData);
-      // do view stuff with the data here
+      let myText2 = "";
+
+      myData.map((myData) => {
+        myText2 += myData.strIngredient13 + ", ";
+      });
       break;
 
     case "nameSearch":
